604dfa84dce44d71c4653a8f33e2e9b5325b0c30
vishrut66 <108527286+vishrut66@users.noreply.github.com>
2023-02-28 13:56:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,18 +39,13 @@ app.use('/uploads', express.static(path.join(__dirname, './uploads')))
 //     app.use(morgan('dev'));
 // }
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, './frontend/build')))
+
+   
     
     app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, './frontend/build', "index.html"));
+    response.sendFile(path.resolve(__dirname, "/frontend/build", "index.html"));
 });
 
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running....')
-    })
-}
 
 // globel error handel middleware
 app.use(errorHandel);
