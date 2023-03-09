2a3fc3d5cf50cb8f838902c03bf5220f43623cd3
vishrut66 <108527286+vishrut66@users.noreply.github.com>
2023-03-09 17:55:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,9 +43,9 @@ app.use('/uploads', express.static(path.join(__dirname, './uploads')))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, './frontend/build')))
 
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, "./frontend/build", 'index.html'))
-    )
+    app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './frontend/build' , "index.html"));
+})
 } else {
     app.get('/', (req, res) => {
         res.send('API is running....')
